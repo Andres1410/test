80f681102491878e2b08d6713a67e011a737318e
Andres Felipe <57474509+Andres1410@users.noreply.github.com>
2024-01-19 10:52:45 -0500

    Update index.jsx

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -3,6 +3,7 @@ import Layout from '../../Components/Layout'
 function NotFound() {
   return (
     <Layout>
+{/*       imagen fondos insuficientes no existe pagina */}
       <img src="https://img.freepik.com/vector-premium/ilustracion-pagina-error-pagina-error-404-concepto-paginas-inicio-isometricas-encabezados-web-pagina-web-construccion_100333-52.jpg?w=740" alt="error" />
     </Layout>
   )
